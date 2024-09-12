668db3ed0dffb9674d71f1fbb2da629d636ff620
k6zma <87338187+k6zma@users.noreply.github.com>
2024-09-12 11:09:06 +0300

    chore: added a json file containing words for the game in English and Russian

---
+++ pkg/utils/json_loader.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"io"
+	"os"
+)
+
+func ReadJSON(filename string) ([]byte, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	byteValue, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return byteValue, err
+}
