e9103fc4214ff00f5dc80b3bbc63c09097b3c5f5
k6zma <87338187+k6zma@users.noreply.github.com>
2024-09-13 23:50:36 +0300

    chore: added comments to improve code readability

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -1,9 +1,20 @@
 package domain
 
+// WordLoaderError defines a custom error of word loading
+//
+// Fields:
+// - message: the error message that describes the issue
 type WordLoaderError struct {
 	message string
 }
 
+// NewWordLoaderError is constructor which creates and returns a new WordLoaderError
+//
+// Parameters:
+// - message: a string containing the error message
+//
+// Function returns:
+// - error: a new instance of WordLoaderError
 func NewWordLoaderError(message string) error {
 	return &WordLoaderError{
 		message: message,
